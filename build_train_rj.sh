cc7e378c7b1b58b36fd88b8bcc68093bbe3d69ce
rgarcia <ramongarcia@aluno.cefet-rj.br>
2025-12-01 01:03:32 +0000

    before opt

--- build_train_rj.sh
+++ build_train_rj.sh
@@ -4,8 +4,8 @@
 # python src/data_handling/build_dataset.py --config /home/rgarcia/mcs/config/config_rj_weekly.yaml
 
 #python src/train_pipeline.py --config config/train_rj_weekly_casesonly.yaml
-python src/train_pipeline.py --config config/train_rj_daily_casesonly.yaml
-python src/train_pipeline.py --config config/train_rj_daily.yaml
+#python src/train_pipeline.py --config config/train_rj_daily_casesonly.yaml
+#python src/train_pipeline.py --config config/train_rj_daily.yaml
 # python src/train_pipeline.py --config config/train_rj_weekly.yaml
 
-#cd /home/rgarcia/mcs ; /usr/bin/env /home/rgarcia/.local/share/virtualenvs/mcs-GNwBDKbT/bin/python /home/rgarcia/mcs/src/optimization_pipeline.py 
+cd /home/rgarcia/mcs ; /usr/bin/env /home/rgarcia/.local/share/virtualenvs/mcs-GNwBDKbT/bin/python /home/rgarcia/mcs/src/optimization_pipeline.py 
